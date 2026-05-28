cca1a6391c6a0554723886adaf67586a6da1343d
Catalin Muresian <catalinmuresian@yahoo.com>
2026-05-28 14:20:39 +0300

    test access page

--- src/pages/modules/AccessPage.vue
+++ src/pages/modules/AccessPage.vue
@@ -1,8 +1,26 @@
 <template>
-  <q-page class="q-pa-md">
-    <div>Access</div>
+  <q-page class="q-pa-md access-page">
+    <q-input v-model="input" autofocus dense flat borderless style="pointer-events: none"/>
+    <h4 class="text-center text-white">Ready to scan</h4>
+    <pre>{{ input }}</pre>
+
   </q-page>
 </template>
 
 <script setup>
+
+import {ref, watch} from "vue";
+
+const input = ref('')
+
+watch(() => input.value, (value) => {
+  console.log(value)
+})
 </script>
+
+<style lang="scss">
+.access-page {
+  background-color: #333;
+}
+
+</style>
